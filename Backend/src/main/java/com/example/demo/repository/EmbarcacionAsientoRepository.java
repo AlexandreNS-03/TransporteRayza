91a195654e4d89dde5b9303046689eb3e31602ab
fabri <fabricionusanchez@gmail.com>
2026-07-11 23:31:46 -0500

    CAMBIOS bd

--- Backend/src/main/java/com/example/demo/repository/EmbarcacionAsientoRepository.java
+++ Backend/src/main/java/com/example/demo/repository/EmbarcacionAsientoRepository.java
@@ -2,13 +2,19 @@ package com.example.demo.repository;
 
 import com.example.demo.model.EmbarcacionAsiento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
 
 @Repository
 public interface EmbarcacionAsientoRepository extends JpaRepository<EmbarcacionAsiento, String> {
     List<EmbarcacionAsiento> findByEmbarcacionIdOrderByNumeroAsc(String embarcacionId);
     List<EmbarcacionAsiento> findByEmbarcacionId(String embarcacionId);
+
+    @Modifying
+    @Transactional
     void deleteByEmbarcacionId(String embarcacionId);
 }
